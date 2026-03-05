7f3d7332aba159b7000c1aa7ae9da6f93030c099
Hilaltbk1 <hilaltabak2021@gmail.com>
2026-03-05 16:19:32 +0300

    Changes environment

--- backend/config/configuration.py
+++ backend/config/configuration.py
@@ -1,9 +1,23 @@
 import os
 from dotenv import load_dotenv
 
+
+
 load_dotenv()
 
+
 class Settings:
+    # os.getenv'in ikinci parametresini None yapalım ki eksik olduğunu anlayalım
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    # Diğerleri kalsın...
+
+    def __init__(self):
+        # Uygulama başlarken kontrol et
+        if not self.DATABASE_URL:
+            raise ValueError(
+                "!!! KRİTİK HATA: DATABASE_URL Environment Variable bulunamadı veya boş. Render panelini kontrol et !!!")
+
 
     GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
 
@@ -11,7 +25,6 @@ class Settings:
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
     # Dosya Yolları
     DOCUMENT_PATH = os.getenv("DOCUMENT_PATH", "./Data/Json/mevzuat_verileri.json")
-    DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
     LLM_MODEL_NAME = os.getenv(
         "LLM_MODEL_NAME","gemini-1.5-flash"
     )
